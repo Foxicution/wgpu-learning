7275daa491df34d210497c4d537c481c4ad334a5
Foxicution <foxicution@gmail.com>
2025-02-28 21:50:29 +0200

    draw a texture using bind groups

--- src/vertex.rs
+++ src/vertex.rs
@@ -6,7 +6,7 @@ use wgpu::{BufferAddress, VertexAttribute, VertexBufferLayout, VertexFormat, Ver
 #[derive(Copy, Clone, Debug, Pod, Zeroable)]
 pub struct Vertex {
     position: [f32; 3],
-    color: [f32; 3],
+    tex_coords: [f32; 2],
 }
 
 impl Vertex {
@@ -25,7 +25,7 @@ impl Vertex {
                 VertexAttribute {
                     offset: size_of::<[f32; 3]>() as BufferAddress,
                     shader_location: 1,
-                    format: VertexFormat::Float32x3,
+                    format: VertexFormat::Float32x2,
                 },
             ],
         }
@@ -34,11 +34,11 @@ impl Vertex {
 
 #[rustfmt::skip]
 pub const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 1.0, 0.5] },
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 1.0] },
-    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [1.0, 0.0, 0.5] },
+    Vertex {position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614]}, // A
+    Vertex {position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354]}, // B
+    Vertex {position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397]}, // C
+    Vertex {position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914]}, // D
+    Vertex {position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641]}, // E
 ];
 
 #[rustfmt::skip]
